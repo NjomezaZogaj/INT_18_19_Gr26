c57164de86cbc9508f0a35c26062359151935b43
NjomezaZogaj <44601526+NjomezaZogaj@users.noreply.github.com>
2018-12-01 15:36:27 +0100

    Update fashion.js

--- Internet_projekti/public_html/fashion.js
+++ Internet_projekti/public_html/fashion.js
@@ -1,2 +1,23 @@
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $(".panel1").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $(".panel1").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $(".panel1").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#flip4").click(function(){
+        $(".panel1").slideToggle("slow");
+    });
+});
 
 
